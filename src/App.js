c02667151e385134a0cb0729ee05de81da382e7b
R-M-G <ryan.gregory@ryanmgregory.com>
2020-10-06 22:52:22 -0500

    rendered API data with map and assigned div key using id

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 
-import Posts from "./components/Posts";
+import Posts from './components/Posts';
+import PostForm from './components/PostForm';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <PostForm />
         <Posts />
-        
       </div>
-    )
+    );
   }
 }
 
 export default App;
-
