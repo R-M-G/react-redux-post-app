05e20344ed927636443beff14726e120a3269e4e
R-M-G <ryan.gregory@ryanmgregory.com>
2020-10-10 23:10:14 -0500

    using connect to send props via redux. working on error message

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,4 +1,4 @@
-import { FETCH_POSTS, NEW_POSTS } from '../actions/types';
+import { FETCH_POSTS, NEW_POST } from '../actions/types';
 
 const initialState = {
   items: [],
@@ -8,6 +8,7 @@ const initialState = {
 export default function (state = initialState, action) {
   switch (action.type) {
     case FETCH_POSTS:
+      
       return {
         ...state,
         items: action.payload,
