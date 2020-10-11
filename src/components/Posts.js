ed8838f8cf0da2de491e5069984bee57995b29e0
R-M-G <ryan.gregory@ryanmgregory.com>
2020-10-10 22:49:17 -0500

    moved fetch to actions folder inside postActions

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -5,11 +5,7 @@ class Posts extends Component {
     posts: [],
   };
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((res) => res.json())
-      .then((data) => this.setState({ posts: data }));
-  }
+ 
   render() {
     const postItems = this.state.posts.map((post) => (
       <div key={post.id}>
